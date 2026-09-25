36dccbb5257fb476d3f6529b446af1b2813a4895
Piersen Matthew George <piersen@unai.edu>
2026-09-25 09:57:51 +0700

    memasukan nilai praktikum

---
+++ src/NilaiAkhir.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NilaiAkhir {
+}
+    public static void main(String[] agrs) {
+        Scanner input = new Scanner(System.in);
+
+        final double BOBOT_PRAKTIKUM = 0.30;
+        final double BOBOT_TUGAS = 0.20;
+        final double BOBOT_MID = 0.20;
+        final double BOBOT_FINAL = 0.30;
+
+        System.out.print("Nilai Praktikum : ");
+        double praktikum = input.nextDouble();
+
+
+
+
+    }
